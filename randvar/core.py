27f200251ebd994ce6879665da78af72d62e4130
BuildTools <unconfigured@null.spigotmc.org>
2017-04-18 17:33:45 -0500

    Documented rand_apply algorithm

--- randvar/core.py
+++ randvar/core.py
@@ -148,12 +148,15 @@ def rand_apply(func, *args, **kwargs):
     constant distributions.
     """
 
+    # Convert all `args` to `RandomVariable`s if they aren't already
     rand_args = []
     for arg in args:
         if isinstance(arg, RandomVariable):
             rand_args.append(arg)
         else:
             rand_args.append(RandomVariable({arg: 1}))
+
+    # Convert all `kwargs` to `RandomVariable`s if they aren't already
     ordered_names = [name for name in kwargs]
     rand_kwargs = {}
     for name in kwargs:
@@ -161,34 +164,42 @@ def rand_apply(func, *args, **kwargs):
             rand_kwargs[name] = kwargs[name]
         else:
             rand_kwargs[name] = RandomVariable({kwargs[name]: 1})
+
+    # Compute the new distribution
     dist = {}
     for args_items in itertools.product(
             *tuple(var._dist.items() for var in rand_args)):
+        # Get the tuple of arguments and their weights for this iteration
         if len(args_items) > 0:
-            args_inst, args_prob = tuple(zip(*args_items))
+            args_inst, args_wts = tuple(zip(*args_items))
             args_inst = deepcopy(args_inst)
         else:
             args_inst = []
-            args_prob = []
+            args_wts = []
         for kwargs_items in itertools.product(
                 *(tuple(rand_kwargs[name]._dist.items()
                         for name in ordered_names))):
+            # Get the dict of keyword arguments and their weights for
+            # this iteration
             if len(kwargs_items) > 0:
-                kwargs_inst, kwargs_prob = tuple(zip(*kwargs_items))
+                kwargs_inst, kwargs_wts = tuple(zip(*kwargs_items))
                 kwargs_inst = {name: deepcopy(kwargs_inst[i]) for i, name in
                                enumerate(ordered_names)}
             else:
                 kwargs_inst = {}
-                kwargs_prob = []
-            prob = functools.reduce(operator.mul,
-                                    itertools.chain(args_prob, kwargs_prob),
+                kwargs_wts = []
+
+            # Compute new weight and incorporate into `dist`
+            weight = functools.reduce(operator.mul,
+                                    itertools.chain(args_wts, kwargs_wts),
                                     1)
-            if prob > 0:
+            if weight > 0:
                 val = func(*args_inst, **kwargs_inst)
                 if val not in dist:
-                    dist[val] = prob
+                    dist[val] = weight
                 else:
-                    dist[val] += prob
+                    dist[val] += weight
+
     return RandomVariable(dist)
 
 
